82c1f9e00985c0700954b6ac8cc32f3bfd25b241
hieunm6 <hieunmce@gmail.com>
2016-09-18 23:21:29 +0700

    add feature document

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-let argv = require('yargs')
-  .usage('node ./index.js [options]')
-  .default('host', '127.0.0.1')
-  .argv
 let proxyServer = require('./proxy')
+let argv = require('yargs')
+  .usage('Usage: node ./index.js [options]')
+  .alias('x', 'host')
+  .describe('x', 'Specify a forwarding host')
+  .default('host', '127.0.0.1')
+  .alias('p', 'port')
+  .describe('p', 'Specify a forwarding port')
+  .alias('l', 'log')
+  .describe('l', 'Specify a output log file')
+  .alias('u', 'url')
+  .describe('u', 'Specify a full URL')
+  .help('h')
+  .alias('h', 'help')
+  .argv
+
 proxyServer(argv);
 
